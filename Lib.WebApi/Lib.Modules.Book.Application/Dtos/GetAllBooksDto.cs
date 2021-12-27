fe0aa596b33d86c8469a9e9a4d06b250384dd1eb
Dominika Góralczyk <dominika.goralczyk@euvic.pl>
2021-12-27 12:06:19 +0100

    dodanie wstepnego mapowania ksiazek

---
+++ Lib.WebApi/Lib.Modules.Book.Application/Dtos/GetAllBooksDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Modules.Book.Application.Dtos
+{
+    public class GetAllBooksDto
+    {
+        public List<BookDetailsDto> BookDetails { get; set; }
+    }
+}
---
+++ Lib.WebApi/Lib.Modules.Book.Application/Mapping/BookProfile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Lib.Modules.Book.Application.Dtos;
+using Lib.Shared.Data.Entities;
+
+namespace Lib.Modules.Book.Application.Mapping;
+
+public class BookProfile : Profile
+{
+    public BookProfile()
+    {
+        CreateMap<Lib.Shared.Data.Entities.Book, BookDetailsDto>()
+            .ForMember(book => book.BookId,
+                opt => opt.MapFrom(x => x.BookId))
+            .ForMember(book => book.Title,
+                opt => opt.MapFrom(x => x.Title))
+            .ForMember(book => book.AuthorFirstName,
+                opt => opt.MapFrom(x => x.Author.FirstName))
+            .ForMember(book => book.AuthorLastName,
+                opt => opt.MapFrom(x => x.Author.LastName))
+            .ForMember(book => book.IsAvailable,
+                opt => opt.MapFrom(x => x.IsAvailable));
+    }
+}
